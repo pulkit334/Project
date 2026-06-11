4333c17ece333ee89310230d84530242d5154012
Pulkit Choudhary <gpulkit744@gmail.com>
2026-06-11 23:03:16 +0530

    Hardcode backend URL in frontend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://backend-ff8c.onrender.com/api',
+  baseURL: 'https://backend-ff8c.onrender.com/api',
   withCredentials: true,
 });
 
